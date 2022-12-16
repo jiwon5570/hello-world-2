96ad2926119faa78ae2198a870a14f7406c1df8e
jiwon5570 <114650067+jiwon5570@users.noreply.github.com>
2022-12-16 16:49:00 +0900

    commit

---
+++ hello/world/main/Application.java
@@ -0,0 +1,8 @@
+package hello.world.main;
+
+public class Application
+    extends Presentation
+    implements Serializable, Cloneable {
+
+    Long id;
+}
--- hello/world/main/BusinessLogic.java
+++ hello/world/main/BusinessLogic.java
@@ -3,6 +3,8 @@ package hello.world.main;
 public class BusinessLogic {
 
     Long id;
+    Address address;
+    Application application;
 
     public static void main(String args[]) {}
 }
